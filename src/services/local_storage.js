331a1df6af809b9f892285e88195bba4ebc63bcf
Kuba Turek <kubus1990@gmail.com>
2015-04-03 13:04:07 +0200

    Change method names and fix bug with JSON.parse

--- src/services/local_storage.js
+++ src/services/local_storage.js
@@ -2,18 +2,21 @@ var app = angular.module('nukomeet.bumel');
 
 app.factory('LocalStorageService', ['$window', function ($window) {
     return {
-        get: function(key) {
-            return $window.localStorage.getItem(JSON.parse(key)) || null;
+        getItem: function (key) {
+            return JSON.parse($window.localStorage.getItem(key)) || null;
         },
-        set: function(key, value) {
+        setItem: function (key, value) {
             return $window.localStorage.setItem(key, JSON.stringify(value));
+        },
+        isSet: function (key) {
+            return this.getItem(key) !== null;
         },
         appendTo: function (key, value) {
             if (!$window.localStorage.getItem(key)) {
                 if (typeof value === "object") {
-                    this.set(key, {});
+                    this.setItem(key, {});
                 } else if (Array.isArray && Array.isArray(value)) {
-                    this.set(key, []);
+                    this.setItem(key, []);
                 }
             }
         }
